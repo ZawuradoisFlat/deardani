d20f632badfee3f11be88bb11cb6ebd81584d1b5
ZawuradoisFlat <luisg5901@gmail.com>
2026-02-06 16:17:22 -0700

    Update config.js

--- config.js
+++ config.js
@@ -62,7 +62,7 @@ animations: {
 music: {
     enabled: true, // Music feature is enabled
     autoplay: true, // Try to autoplay (note: some browsers may block this)
-    musicUrl: "https://tidal.com/track/227217541/u", // Paste your music URL here
+    musicUrl: "", // Paste your music URL here
     startText: "🎵 Play Music", // Button text to start music
     stopText: "🔇 Stop Music", // Button text to stop music
     volume: 0.5 // Volume level (0.0 to 1.0)
